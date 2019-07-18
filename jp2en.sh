9c52f4b79a6899abca7c87a24ee293c318daacc5
AndersenJ <johnathan.andersen@byu.edu>
2019-07-17 19:13:53 -0600

    screwed around a little, but it didn't fix anything

--- jp2en.sh
+++ jp2en.sh
@@ -5,7 +5,8 @@ tesseract jp.png tout -l jpn 2> /dev/null
 sed 's/ //g' tout.txt > tout2.txt
 cat tout2.txt
 #trans -brief "`cat tout2.txt`"
-sleep 1
-trans -s 日本語 --show-original n "`cat tout2.txt`"
-sleep 5
+#sleep 1
+#trans -s 日本語 --show-original n "`cat tout2.txt`"
+trans -s 日本語 -brief --show-original Y -i tout2.txt
+sleep 2
 rm tout.txt tout2.txt jp.png
