5c9481e8391aa49c1b70d7806e3380ee97768844
AndersenJ <johnathan.andersen@byu.edu>
2020-02-06 16:04:27 -0700

    added to the banlist

--- tokenize.py
+++ tokenize.py
@@ -9,7 +9,7 @@ tokenized_statement = tinysegmenter.tokenize(statement)
 #print(tokenized_statement)
 
 banned_characters = ["①"]
-banlist = ["", " ", "は", "の", "で", "だっ", "する", "、", "", "。", "て", "が", "に", "お", "と", "よう", "な", "!", "い", "だ", "た", "<", ">", "一"]
+banlist = ["", " ", "は", "の", "で", "だっ", "する", "、", "", "。", "て", "が", "に", "お", "と", "よう", "な", "!", "い", "だ", "た", "<", ">", "一", "う"]
 
 for token in tokenized_statement:
     if token not in banlist:
